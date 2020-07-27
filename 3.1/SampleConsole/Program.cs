0faedb097c161303efd9aa2a528a7e48290d859e
bfeng <bfeng@nltechdev.com>
2020-07-27 15:11:53 +0800

    Update Console

--- 3.1/SampleConsole/Program.cs
+++ 3.1/SampleConsole/Program.cs
@@ -8,18 +8,18 @@ namespace SampleConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"There are {args.Length} arguments.");
 
             // loop through the assemblies that this app references
             foreach (var r in Assembly.GetEntryAssembly()
             .GetReferencedAssemblies())
             {
                 // load the assembly so we can read its details
-                var a = Assembly.Load(new AssemblyName(r.FullName));
+                var assem = Assembly.Load(new AssemblyName(r.FullName));
                 // declare a variable to count the number of methods
                 int methodCount = 0;
                 // loop through all the types in the assembly
-                foreach (var t in a.DefinedTypes)
+                foreach (var t in assem.DefinedTypes)
                 {
                     // add up the counts of methods
                     methodCount += t.GetMethods().Count();
@@ -27,10 +27,27 @@ namespace SampleConsole
                 // output the count of types and their methods
                 Console.WriteLine(
                 "{0:N0} types with {1:N0} methods in {2} assembly.",
-                arg0: a.DefinedTypes.Count(),
+                arg0: assem.DefinedTypes.Count(),
                 arg1: methodCount,
                 arg2: r.Name);
             }
+
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+
+
+            double a = 0.1;
+            double b = 0.2;
+            if (a + b == 0.3)
+            {
+                Console.WriteLine($"{a} + {b} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {b} does NOT equal 0.3, it equals to {a + b}");
+            }
+
         }
     }
 }
