fae39ee2f591e6cdbafc79c411f7f69b3b9dd244
bfeng <bfeng@nltechdev.com>
2020-08-04 10:53:16 +0800

    daily commit

--- 3.1/SampleConsole/Program.cs
+++ 3.1/SampleConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Microsoft.Extensions.Configuration;
 using static System.Console;
 using static SampleConsole.Math;
 
@@ -14,9 +15,28 @@ namespace SampleConsole
         {
             Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
             Trace.AutoFlush = true;
-            WriteLine($"Max Value Interger is {Int32.MaxValue}");
+
             Debug.WriteLine("Debug says, I am watching!");
             Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json",
+                optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(
+                displayName: "PacktSwitch",
+                description: "This switch is set via a JSON config.");
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+            
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
             RunFactorial();
 
             // loop through the assemblies that this app references
