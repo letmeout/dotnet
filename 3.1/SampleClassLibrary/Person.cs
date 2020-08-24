a11ce6529128beaf519ef512dfbd0ecda0132bcd
bfeng <bfeng@nltechdev.com>
2020-08-24 14:11:07 +0800

    daily build

--- 3.1/SampleClassLibrary/Person.cs
+++ 3.1/SampleClassLibrary/Person.cs
@@ -4,6 +4,31 @@ namespace SampleClassLibrary
 {
     public class Person
     {
-        public string Name { get; set; }
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+        // methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        // static method to "multiply"
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+        // instance method to "multiply"
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
     }
 }
